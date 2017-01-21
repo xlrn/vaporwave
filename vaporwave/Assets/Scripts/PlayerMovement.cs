8af26f1f4a095ecfe6854891849df70cb5cfac86
Graham <gramclee@gmail.com>
2017-01-20 22:01:00 -0800

    Edited input manager settings to account for seperate player controls. Also edited player scripts to account for this

--- vaporwave/Assets/Scripts/PlayerMovement.cs
+++ vaporwave/Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,11 @@ public class PlayerMovement : MonoBehaviour {
     private float xMovement;
     private float yMovement;
     private Rigidbody2D rb;
+    public string horizontalControl = "Horiz_P1";
+    public string verticalControl = "Vert_P1";
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
         rb = GetComponent<Rigidbody2D>();
 		
@@ -18,8 +20,8 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        xMovement = Input.GetAxisRaw("Horizontal");
-        yMovement = Input.GetAxisRaw("Vertical");
+        xMovement = Input.GetAxisRaw(horizontalControl);
+        yMovement = Input.GetAxisRaw(verticalControl);
         GetComponent<Rigidbody2D>().velocity = new Vector2(xMovement * speed, yMovement * speed);
 
     }
