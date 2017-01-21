4d83f5e9896db9c1b41f6be68f38b0658cc0c785
xlrn <ncl.yim@gmail.com>
2017-01-20 21:25:13 -0800

    Implemented HP for cities

---
+++ vaporwave/Assets/Scripts/CityController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityController : MonoBehaviour {
+
+    private int hp;
+    private static int maxHp;
+
+    void Start() {
+        hp = maxHp;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Projectile")
+        {
+            hp--;
+            print(hp + "\n");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
