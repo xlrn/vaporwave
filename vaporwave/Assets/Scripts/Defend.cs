b78e511382b80f551aa21d9200876befba23e320
Graham <gramclee@gmail.com>
2017-01-21 10:44:08 -0800

    Fixed bug that caused both players to block

--- vaporwave/Assets/Scripts/Defend.cs
+++ vaporwave/Assets/Scripts/Defend.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Defend : MonoBehaviour {
-    public string defenseInput = "q";
+    public string defenseInput;
     private Animator anim;
     private bool isDefending = false;
     public Collider2D defenseTrigger;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         anim = gameObject.GetComponent<Animator>();
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
        
-            bool isDefending = Input.GetKey(defenseInput);
-            anim.SetBool("isDefending", isDefending);
-        if (isDefending)
+        if (Input.GetKey(defenseInput))
         {
             defenseTrigger.enabled = true;
+            anim.SetBool("isDefending", true);
         }
         else {
             defenseTrigger.enabled = false;
+            anim.SetBool("isDefending", false);
         }
     }
 
