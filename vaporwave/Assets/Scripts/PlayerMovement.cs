628276bd5f6ac8eb2c4eee96d694bd9c20419efd
Graham <gramclee@gmail.com>
2017-01-20 21:17:27 -0800

    Created test player prefab and wrote script that allows player movement

---
+++ vaporwave/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private float xMovement;
+    private float yMovement;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        xMovement = Input.GetAxisRaw("Horizontal");
+        yMovement = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(xMovement * speed, yMovement * speed);
+
+    }
+}
