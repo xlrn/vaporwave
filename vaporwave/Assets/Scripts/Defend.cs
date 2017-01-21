e5fd0af30cf7d110b4f4900c51bd1ff7001f6f05
Graham <gramclee@gmail.com>
2017-01-21 01:08:40 -0800

    Added defense script and several test animations

---
+++ vaporwave/Assets/Scripts/Defend.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defend : MonoBehaviour {
+    public string defenseInput = "Def1";
+    private Animator anim;
+    private bool isDefending = false;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+            bool isDefending = Input.GetKey("q");
+            anim.SetBool("isDefending", isDefending);
+
+
+	}
+
+    void Defense() {
+
+
+
+    }
+}
