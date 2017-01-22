08b68cd8b818d41ba8185b5bf6d9958b126583ca
xlrn <ncl.yim@gmail.com>
2017-01-22 11:06:38 -0800

    Added more audio

---
+++ vaporwave/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+
+    public string BackgroundMusicAudio;
+
+    // Use this for initialization
+    void Start () {
+        SoundManager.Play(BackgroundMusicAudio);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
